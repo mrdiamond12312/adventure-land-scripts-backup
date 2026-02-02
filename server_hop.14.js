1bf3fcf60825fa4458bb0ee0bdf02ef16f06c16e
Nice <hoangkimc93@gmail.com>
2026-02-02 22:51:42 +0700

    chore(pinkgoo): adjust serverhop

--- server_hop.14.js
+++ server_hop.14.js
@@ -7,7 +7,7 @@ const HOME_SERVER = {
   serverIdentifier: "II",
 };
 
-const tankableBoss = ["snowman", "pinkgoo"];
+const tankableBoss = ["snowman"];
 
 const bosses = {
   grinch: { type: "grinch", threshold: 0.7, hoppable: 1 },
@@ -17,6 +17,7 @@ const bosses = {
   mrgreen: { type: "mrgreen", threshold: 0.3, hoppable: 0.9999 },
   crabxx: { type: "crabxx", threshold: 0.95, hoppable: 0.9999 },
   dragold: { type: "dragold", threshold: 0.5, hoppable: 1 },
+  pinkgoo: { type: "pinkgoo", threshold: 0.5, hoppable: 0.99 },
 };
 const waitForEvent = ["wabbit"];
 
