49c21e9376b615f2dae23f3b411b813ea73c0141
Nice <hoangkimc93@gmail.com>
2026-02-19 22:34:57 +0700

    chore(blink): preserve some more mana for blinking

--- strategic_smart_move.21.js
+++ strategic_smart_move.21.js
@@ -467,7 +467,7 @@ class StrategicSmartMove {
           if (
             blinkLocation &&
             !is_on_cooldown("blink") &&
-            character.mp > parent.G.skills["magiport"].mp * 2 &&
+            character.mp > parent.G.skills["blink"].mp * 2 &&
             distance(character, { x: blinkLocation.x, y: blinkLocation.y }) >
               200 &&
             character.mp > parent.G.skills["blink"].mp
