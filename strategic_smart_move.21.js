67d97ab62aeaeb07b4585b406292f23aab7457d5
Nice <hoangkimc93@gmail.com>
2026-04-05 18:48:08 +0700

    chore: add back transport with retry

--- strategic_smart_move.21.js
+++ strategic_smart_move.21.js
@@ -320,40 +320,40 @@ class StrategicSmartMove {
     });
   }
 
-  async transport(map, spawn) {
-    const waitPromise = this.waitForNewMap();
-    parent.socket.emit("transport", { to: map, s: spawn });
-    parent.push_deferred("transport");
-
-    try {
-      await waitPromise;
-    } catch (error) {
-      console.warn("Transport timeout! Current map:", character.map);
-    }
-  }
-
   // async transport(map, spawn) {
   //   const waitPromise = this.waitForNewMap();
   //   parent.socket.emit("transport", { to: map, s: spawn });
-
-  //   const catchPromise = parent.push_deferred("transport").catch(async (e) => {
-  //     if (e.response === "transport_cant_reach") {
-  //       const door = this._findDoorTo(map);
-  //       if (door) {
-  //         await smart_move(door.x, door.y);
-  //         await this.transport(map, spawn);
-  //       }
-  //     }
-  //   });
+  //   parent.push_deferred("transport");
 
   //   try {
   //     await waitPromise;
   //   } catch (error) {
   //     console.warn("Transport timeout! Current map:", character.map);
   //   }
-
-  //   await catchPromise;
   // }
+
+  async transport(map, spawn) {
+    const waitPromise = this.waitForNewMap();
+    parent.socket.emit("transport", { to: map, s: spawn });
+
+    const catchPromise = parent.push_deferred("transport").catch(async (e) => {
+      if (e.response === "transport_cant_reach") {
+        const door = this._findDoorTo(map);
+        if (door) {
+          await smart_move(door.x, door.y);
+          await this.transport(map, spawn);
+        }
+      }
+    });
+
+    try {
+      await waitPromise;
+    } catch (error) {
+      console.warn("Transport timeout! Current map:", character.map);
+    }
+
+    await catchPromise;
+  }
 
   /**
    * @param {string | Object} toPosition - the monster id or map or coordinates object to move to
