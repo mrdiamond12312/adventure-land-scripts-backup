18da2f32e01ce002ada8b4a6ae40d1d8e5e464eb
Nice <hoangkimc93@gmail.com>
2026-07-08 23:36:12 +0700

    chore(priest): not moving to healee anymore

--- basic_function.7.js
+++ basic_function.7.js
@@ -156,7 +156,7 @@ var mapY = -301;
 //   "turtle",
 //   "crabx",
 // ];
-var mobsToFarm = ["ent", "plantoid"];
+var mobsToFarm = ["ent", "plantoid", "mechagnome"];
 // var mobsToFarm = ["prat"];
 // var mobsToFarm = ["mummy"];
 // var mobsToFarm = ["jr", "booboo"];
