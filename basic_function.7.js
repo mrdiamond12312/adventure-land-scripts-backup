1e169192367a80ff787d19650ce72e36e577bb03
Nice <hoangkimc93@gmail.com>
2026-07-23 00:09:48 +0700

    chore(ent): decrease the cap to 2, since the warrior can't sustain 3 yet

--- basic_function.7.js
+++ basic_function.7.js
@@ -427,7 +427,6 @@ const SALE_ABLE = [
   "vgloves",
   "mcape",
   "wbook0",
-  "quiver",
   "santasbelt",
   "mushroomstaff",
   "slimestaff",
@@ -1696,7 +1695,7 @@ const setRogueSpeedLastDeployment = () => {
   set("rogueLastDeployed", new Date());
 };
 
-const ENT_FIELD_MAX_FOR_ROGUE = 2;
+const ENT_FIELD_MAX_FOR_ROGUE = 1;
 const ENT_FIELD_STALE_MS = 15 * 1000;
 
 const shouldDeployRogue = () => {
